064a946b079b99bbf47961a92933aa8dd4d13ded
Lea Penarubia <princesslaion@gmail.com>
2024-08-14 20:33:25 +1000

    Connection to in-memory H2 DB

--- src/main/java/com/lrp/springboot/learn_spring_boot/model/User.java
+++ src/main/java/com/lrp/springboot/learn_spring_boot/model/User.java
@@ -2,6 +2,9 @@ package com.lrp.springboot.learn_spring_boot.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,7 +13,10 @@ import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name is Minimum of 2 characters required")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user_details (id, birth_Date, name, confidential_data)
+values (100001, current_date(), 'John Smith', 'yale');
+
+insert into user_details (id, birth_Date, name, confidential_data)
+values (100002, current_date(), 'Jane Smith', 'harvard');
